f222e0196e50013f1a94782cd2337390f33cf53c
Ronald Kasaija <615630@cs.mum.edu>
2023-06-20 14:25:08 -0500

    Removed client discovery

--- jdrs/src/main/java/com/group9/jdrs/JdrsApplication.java
+++ jdrs/src/main/java/com/group9/jdrs/JdrsApplication.java
@@ -2,12 +2,10 @@ package com.group9.jdrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
-//@EnableDiscoveryClient
 public class JdrsApplication {
 
 	public static void main(String[] args) {

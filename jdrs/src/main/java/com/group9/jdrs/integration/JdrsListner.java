5c4815eee5c7dd080163cd72626ca2950584cc02
Kevin Chemutai <615620@cs.mum.edu>
2023-06-20 23:29:35 -0500

    add splitter functionality

--- jdrs/src/main/java/com/group9/jdrs/integration/JdrsListner.java
+++ jdrs/src/main/java/com/group9/jdrs/integration/JdrsListner.java
@@ -3,6 +3,7 @@ package com.group9.jdrs.integration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,10 +20,28 @@ public class JdrsListner {
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public JdrsListner() {
+	}
+
 	@KafkaListener(topics = "RTDIS_IOWA", groupId = "group-id")
 	public void receiveWeatherData(String weatherJson) {
 		try {
 			WeatherDTO weatherDTO = objectMapper.readValue(weatherJson, WeatherDTO.class);
+
+			//Rip data
+			WeatherDTO.CurrentDTO currentDTO = weatherDTO.getCurrent();
+			WeatherDTO.LocationDTO locationDTO= weatherDTO.getLocation();
+
+			//send to topics
+			String currentDToString = objectMapper.writeValueAsString(currentDTO);
+			kafkaTemplate.send("jdrs_current", currentDToString);
+
+			String locationDToString = objectMapper.writeValueAsString(locationDTO);
+			kafkaTemplate.send("jdrs_location", locationDToString);
+
 		} catch (JsonMappingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
